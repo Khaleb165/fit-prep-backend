e8e54d0c895f0ddb8c8ab7d0885e88686ca44a23
cagyei-quantum <cagyei@quantumgroupgh.com>
2026-06-14 15:44:43 +0000

    feat: Integrate Plan components into main application middleware

--- routes/_middleware.dart
+++ routes/_middleware.dart
@@ -1,11 +1,15 @@
 import 'package:dart_frog/dart_frog.dart';
 import 'package:fit_prep_backend/controllers/auth_controller.dart';
+import 'package:fit_prep_backend/controllers/plan_controller.dart';
+import 'package:fit_prep_backend/repositories/plan_repository.dart';
 import 'package:fit_prep_backend/repositories/user_repository.dart';
 import 'package:fit_prep_backend/services/auth_service.dart';
+import 'package:fit_prep_backend/services/plan_service.dart';
 import 'package:fit_prep_backend/utils/hashing.dart';
 import 'package:fit_prep_backend/utils/jwt.dart';
 
 final UserRepository _userRepository = InMemoryUserRepository();
+final PlanRepository _planRepository = InMemoryPlanRepository();
 final JwtService _jwtService = JwtService(
   config: JwtConfig.fromEnvironment(),
 );
@@ -14,6 +18,9 @@ final AuthService _authService = AuthService(
   passwordHasher: const BCryptPasswordHasher(),
   jwtService: _jwtService,
 );
+final PlanService _planService = PlanService(
+  planRepository: _planRepository,
+);
 
 Handler middleware(Handler handler) {
   return handler
@@ -21,5 +28,8 @@ Handler middleware(Handler handler) {
       .use(provider<UserRepository>((context) => _userRepository))
       .use(provider<JwtService>((context) => _jwtService))
       .use(provider<AuthService>((context) => _authService))
-      .use(provider<AuthController>((context) => const AuthController()));
+      .use(provider<PlanRepository>((context) => _planRepository))
+      .use(provider<PlanService>((context) => _planService))
+      .use(provider<AuthController>((context) => const AuthController()))
+      .use(provider<PlanController>((context) => const PlanController()));
 }
